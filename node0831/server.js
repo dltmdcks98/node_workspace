0ace141a7b5ba95988f73245d3e9b2d50748e3b0
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-31 10:01:08 +0900

    22.08.31 express 사용 서버

--- node0831/server.js
+++ node0831/server.js
@@ -6,4 +6,21 @@
     해결책)http모듈보다 개선된 외부 모듈인 express 모듈을 사용해본다.
 */
 //var은 변수 선언시 var을 쓰면 중복선언을 허용해줌 let은 중복을 허용하지 않음 
-let express = require("express");
+var http = require("http");//내부모듈
+let express = require("express");//외부 모듈
+
+let app = express();//express 객체생성
+
+//express 모듈은 미들웨어라 불리는 함수를 지원하여, 기능을 확장할 수 있다. 
+app.use("/static",express.static("res"));//미들웨어 등록, res라는 디렉토리를 정적자원의 위치로 등록 use(가상주소, static파일이 있는 폴더 )
+
+//express 모듈을 이용할 경우 요청처리도 express 모듈로 처리하면 된다.
+app.get("/main",function(request, response){//(주소,)
+    console.log("클라이언트 요청 감지")
+});
+
+let server = http.createServer(app);
+
+server.listen(8888,function(){
+    console.log("The server is running at 8888 port");
+});
